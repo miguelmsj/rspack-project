6d4d12bbfcb56b55a193bb6a5502c0ec85137577
Miguel San Juan <miguelmsj@gmail.com>
2025-06-04 20:37:07 -0300

    Initialize project with Rspack, React, and TypeScript setup, including configuration files, basic structure, and sample components.

---
+++ rspack.config.ts
@@ -0,0 +1,70 @@
+// import { defineConfig } from '@rspack/cli';
+import { rspack } from '@rspack/core';
+import { ReactRefreshRspackPlugin } from '@rspack/plugin-react-refresh';
+import { withZephyr } from 'zephyr-webpack-plugin';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+// Target browsers, see: https://github.com/browserslist/browserslist
+const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR'];
+
+export default withZephyr()({
+  entry: {
+    main: './src/main.tsx',
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // @ts-expect-error Below are non-blocking error and we are working on improving them
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+    isDev ? new ReactRefreshRspackPlugin() : null,
+  ].filter(Boolean),
+  optimization: {
+    minimizer: [
+      // @ts-expect-error Types for SwcJsMinimizerRspackPlugin are not yet available
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      // @ts-expect-error Types for LightningCssMinimizerRspackPlugin are not yet available
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+  experiments: {
+    css: true,
+  },
+});
